b5c7a627423de62780f784853ee5ca5fc460c76f
AerialRoboticsGroup2 <zhhuan@utu.fi>
2026-05-28 10:57:36 +0300

    Update cnn_controller.py

--- scripts/cnn_controller.py
+++ scripts/cnn_controller.py
@@ -10,7 +10,7 @@ import torch
 import torch.nn as nn
 
 # ======================================
-# CNN MODEL
+# CNNs: learning obstacle avoidance, reactive navigation, motion policy
 # ======================================
 
 class CNNController(nn.Module):
