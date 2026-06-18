990e802fa46da72ed7d7ff6690b1b664886ee8ca
AerialRoboticsGroup2 <zhhuan@utu.fi>
2026-06-18 22:30:01 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,51 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 package_name = 'multi_robot_project'
 
 setup(
     name=package_name,
     version='0.0.1',
-    packages=[package_name],
-    install_requires=['setuptools'],
+
+    packages=find_packages(),
+
+    install_requires=[
+        'setuptools'
+    ],
+
     zip_safe=True,
+
     maintainer='student',
     maintainer_email='student@email.com',
-    description='Multi robot cooperative control',
+
+    description='Multi Robot Cooperative Control Project',
+
     license='MIT',
+
+    tests_require=['pytest'],
+
     entry_points={
         'console_scripts': [
+
             'formation_controller = scripts.formation_controller:main',
+
             'collision_avoidance = scripts.collision_avoidance:main',
+
             'coverage_controller = scripts.coverage_controller:main',
+
             'consensus_node = scripts.consensus_node:main',
+
             'lidar_processor = scripts.lidar_processor:main',
+
             'connectivity_monitor = scripts.connectivity_monitor:main',
-        ],
-    },
-    entry_points={
-    'console_scripts': [
-        'cnn_controller = scripts.cnn_controller:main',
-        'dataset_recorder = scripts.dataset_recorder:main',
+
+            'topology_manager = scripts.topology_manager:main',
+
+            'mission_manager = scripts.mission_manager:main',
+
+            'cnn_controller = scripts.cnn_controller:main',
+
+            'dataset_recorder = scripts.dataset_recorder:main',
+
         ],
     },
 )
