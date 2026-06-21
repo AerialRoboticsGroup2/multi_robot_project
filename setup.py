87e756aa8b1ff325b475f642d6ba914e297cb136
AerialRoboticsGroup2 <zhhuan@utu.fi>
2026-06-21 17:27:37 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,20 +8,16 @@ setup(
 
     packages=find_packages(),
 
-    install_requires=[
-        'setuptools'
-    ],
+    install_requires=['setuptools'],
 
     zip_safe=True,
 
     maintainer='student',
     maintainer_email='student@email.com',
 
-    description='Multi Robot Cooperative Control Project',
+    description='Multi Robot Cooperative Control',
 
     license='MIT',
-
-    tests_require=['pytest'],
 
     entry_points={
         'console_scripts': [
